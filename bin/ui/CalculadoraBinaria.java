51b8f547bc1777f676819bb5bf23ab138718f05f
Joshua Lattke <joshualattke@hotmail.com>
2022-04-01 14:28:24 -0500

    prueba de cambios reflejados

--- bin/ui/CalculadoraBinaria.java
+++ bin/ui/CalculadoraBinaria.java
@@ -2,5 +2,5 @@ package ui;
 import java.util.Scanner
 public class CalculadoBinaria
 Sytem.out.println("ingrese su fecha de nacimiento");
-System.out.println("ingrese su nombre")
-System.out.println("")
+System.out.println("ingrese su nombre");
+System.out.println("ingres su fecha de nacimiento");
