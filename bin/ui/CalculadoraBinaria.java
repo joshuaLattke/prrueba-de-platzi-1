5e1e5abdd7889b45fbce808e0cf0f9ec890fb542
Joshua Lattke <joshualattke@hotmail.com>
2022-04-01 14:13:21 -0500

    primera prueba de github en platzi

---
+++ bin/ui/CalculadoraBinaria.java
@@ -0,0 +1,6 @@
+package ui;
+import java.util.Scanner
+public class CalculadoBinaria
+Sytem.out.println("ingrese su fecha de nacimiento");
+System.out.println("ingrese su nombre")
+System.out.println("")
