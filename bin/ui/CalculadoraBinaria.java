2a1991b988af13dda1e353c8772e1c24e7002f07
danielnxnx <danielnxnx@gmail.com>
2022-04-06 19:32:55 +0200

    prueba de colaboradores

--- bin/ui/CalculadoraBinaria.java
+++ bin/ui/CalculadoraBinaria.java
@@ -1,8 +1,2 @@
-package ui;
-import java.util.Scanner
-public class CalculadoBinaria
-int=fecha_nacimiento
-Sytem.out.println("ingrese su fecha de nacimiento");
-System.out.println("ingrese su nombre");
-System.out.println("ingres su fecha de nacimiento");
-ss
+//prueba de platzi
+//prueba de colaboradores en Githbu
